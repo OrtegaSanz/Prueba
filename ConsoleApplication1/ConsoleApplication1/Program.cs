a400e8c6f317d7d0593a406654d33a13c584a776
Clase <Clase>
2017-03-09 20:36:09 -0500

    Holi

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Write("Esto es un Pack");
+            Console.Write("Holi");
             Console.ReadLine();
         }
     }
